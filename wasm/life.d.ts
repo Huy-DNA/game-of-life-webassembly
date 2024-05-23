0df4f2fe409a94c465f7b985f4b21afa7560c50d
Huy-DNA <huydo862003@gmail.com>
2024-05-23 21:09:17 +0700

    fix: gen wasm for web import

--- wasm/life.d.ts
+++ wasm/life.d.ts
@@ -25,3 +25,34 @@ export class Universe {
 */
   toggle(i: number, j: number): void;
 }
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly __wbg_universe_free: (a: number) => void;
+  readonly universe_new: (a: number, b: number) => number;
+  readonly universe_tick: (a: number) => void;
+  readonly universe_toggle: (a: number, b: number, c: number) => void;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+/**
+* Instantiates the given `module`, which can either be bytes or
+* a precompiled `WebAssembly.Module`.
+*
+* @param {SyncInitInput} module
+*
+* @returns {InitOutput}
+*/
+export function initSync(module: SyncInitInput): InitOutput;
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {InitInput | Promise<InitInput>} module_or_path
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function __wbg_init (module_or_path?: InitInput | Promise<InitInput>): Promise<InitOutput>;
--- wasm/life.js
+++
@@ -1,4 +0,0 @@
-import * as wasm from "./life_bg.wasm";
-import { __wbg_set_wasm } from "./life_bg.js";
-__wbg_set_wasm(wasm);
-export * from "./life_bg.js";
