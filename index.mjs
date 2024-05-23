37adb9d937823cb2ea7ac90b85df2b8dd13ce13c
Huy-DNA <huydo862003@gmail.com>
2024-05-24 02:49:54 +0700

    fix: use prefix ++

--- index.mjs
+++ index.mjs
@@ -37,12 +37,12 @@ function renderGrid() {
 
   ctx.strokeStyle = GRID_COLOR;
 
-  for (let i = 0; i <= width; i++) {
+  for (let i = 0; i <= width; ++i) {
     ctx.moveTo(i * (CELL_SIZE + 1) + 1, 0);
     ctx.lineTo(i * (CELL_SIZE + 1) + 1, (CELL_SIZE + 1) * height + 1);
   }
 
-  for (let j = 0; j <= height; j++) {
+  for (let j = 0; j <= height; ++j) {
     ctx.moveTo(0, j * (CELL_SIZE + 1) + 1);
     ctx.lineTo((CELL_SIZE + 1) * width + 1, j * (CELL_SIZE + 1) + 1);
   }
