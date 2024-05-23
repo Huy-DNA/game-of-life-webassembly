cb9ef4bbb743a0569f1bb2ea8702abbc19447176
Huy-DNA <huydo862003@gmail.com>
2024-05-23 19:39:03 +0700

    chore: fix .gitignore

--- src/target/debug/deps/asm_core-277ab82fc8ca77bc.d
+++
@@ -1,5 +0,0 @@
-/home/hell/projects/life/src/asm-core/target/debug/deps/libasm_core-277ab82fc8ca77bc.rmeta: src/main.rs
-
-/home/hell/projects/life/src/asm-core/target/debug/deps/asm_core-277ab82fc8ca77bc.d: src/main.rs
-
-src/main.rs:
